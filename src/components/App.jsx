2572d62eba2122f805086d0afa7277d04fbfe8c5
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-09 11:09:08 +0100

    update
    Reafctoring DONE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,73 @@
-import { Component } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
 
   //================================ Increse Feedback function
-  onLeaveFeedback = (option) => {
-      this.setState((prevState) => {
-        return {
-          [option]: prevState[option] + 1,
-        }
-      })
+  function onLeaveFeedback(option) {
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        return;
+    }
   }
 
   // Calculate TOTAL quality of feedbacks
-  countTotalFeedback = () => {
+  function countTotalFeedback() {
     let summ = 0;
-    const arrayOfFeedbacks = Object.values(this.state);
+    const arrayOfFeedbacks = [good, neutral, bad];
     for (let i = 0; i < arrayOfFeedbacks.length; i++) {
       summ += arrayOfFeedbacks[i];
     }
     return summ;
-  };
+  }
 
   //===================================== Calculate Percentage of Positive Feedbacks
-  countPositiveFeedbackPercentage = total => {
-    const positivePercent = (this.state.good / total) * 100 || 0;
+  function countPositiveFeedbackPercentage(total) {
+    const positivePercent = (good / total) * 100 || 0;
     return Math.round(positivePercent);
-  };
+  }
 
   //=========================================== Render JSX of feedback form
-  render() {
-    const options = Object.keys(this.state);
-    return (
-      <div>
-        <Section title="please, leave your feedback">
-          <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        <Section title="statistics">
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage(
-                this.countTotalFeedback()
-              )}
-            />
-          ) : (
-            <Notification message="no feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Section title="please, leave your feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage(
+              countTotalFeedback()
+            )}
+          />
+        ) : (
+          <Notification message="no feedback given" />
+        )}
+      </Section>
+    </div>
+  );
+};
